0977eba3778254f76eb23dfb6a246ddba2550dd9
Fran <fran.sipek123@gmail.com>
2025-04-04 23:19:41 +0200

    add header section

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+function MenuItem({
+  title,
+  address,
+  Icon,
+}: {
+  title: string;
+  address: string;
+  Icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+}) {
+  return (
+    <Link href={address} className="hover:text-amber-500  ">
+      <Icon className="text-2xl" />
+      <p className="uppercase hidden sm:inline text-sm">{title}</p>
+    </Link>
+  );
+}
+
+export default MenuItem;
