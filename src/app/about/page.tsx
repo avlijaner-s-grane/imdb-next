7916d4e112a239e6eaa965b2d8f06017a41b3313
Fran <fran.sipek123@gmail.com>
2025-04-05 00:35:06 +0200

    create about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,34 @@
+function page() {
+  return (
+    <div className="items-center max-w-6xl mx-auto space-y-4 p-3">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to our movie databse website! We are a team of passionate movie
+        enthusiasts who have come together to create a one-stop destination for
+        all your movie-related needs.
+      </p>
+      <p>
+        Our website is designed to provide you with a comprehensive database of
+        movies, including information on cast and crew, reviews , ratings, and
+        much more. Wee belive that movies have the power to inspire, entertain,
+        and bring people together, and we are deidacted to sharing that passion
+        with you.
+      </p>
+      <p>
+        In addtion to our extensive movie database, we also offer a platform for
+        movie lovers to connect and share their thoughts on the latest releases.
+        Our community section includes a forum where you can discuss your
+        favorite films with like-minded individuals and read reviews and ratings
+        from other users. We also have a section dedicated to movie news and
+        trailers, keeping you up-to-date with the latest happening in the world
+        of cinema. Thank you for visiting our website and we hope you enjoy your
+        time browsing through our movie database. If you have any feedback or
+        suggestions, please feel free to contact us. We are always looking for
+        ways to improve and enhance the user experience on our website. Happy
+        browsing!
+      </p>
+    </div>
+  );
+}
+
+export default page;
