107e7fecff1ddfc59db972cc703a299f71ebfd6a
Fran Sipek <fran.sipek123@gmail.com>
2026-07-18 05:25:55 +0200

    More fixing for deployment

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import NavbarItem from "./NavbarItem";
-
+import { Suspense } from "react";
 function Navbar() {
   return (
     <div className="mt-10 shadow-lg shadow-gray-800 flex bg-amber-600 text-white p-4 lg:text-lg justify-center gap-6 w-[70%] mx-auto rounded-full ">
-      <NavbarItem title="Trending" param="fetchTrending" />
-      <NavbarItem title="Top Rated" param="fetchTopRated" />
+      <Suspense fallback={<div>Loading...</div>}>
+        <NavbarItem title="Trending" param="fetchTrending" />
+        <NavbarItem title="Top Rated" param="fetchTopRated" />
+      </Suspense>
     </div>
   );
 }
