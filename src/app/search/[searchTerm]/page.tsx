335ddb55def9a2ffca1e1540bcbd2551c4fd2cbe
Fran Sipek <fran.sipek123@gmail.com>
2026-07-18 05:18:31 +0200

    Adjuested a few components for Next 15

--- src/app/search/[searchTerm]/page.tsx
+++ src/app/search/[searchTerm]/page.tsx
@@ -1,22 +1,30 @@
 import Results from "@/components/Results";
 
-async function page({ params }: { params: { searchTerm: string } }) {
-  const { searchTerm } = params;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ searchTerm: string }>;
+}) {
+  const { searchTerm } = await params;
+
   const response = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=false`
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${encodeURIComponent(searchTerm)}&language=en-US&page=1&include_adult=false`,
   );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch search results");
+  }
+
   const data = await response.json();
   const results = data.results;
 
   return (
     <div>
-      {results && results.length === 0 ? (
+      {results.length === 0 ? (
         <h1 className="text-center pt-6">No results found.</h1>
-      ) : null}
-
-      {results && <Results results={results} />}
+      ) : (
+        <Results results={results} />
+      )}
     </div>
   );
 }
-
-export default page;
